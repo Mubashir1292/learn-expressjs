bc573d3476cc018336edf4e95bbbf46d0de8cd0e
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-09 09:27:41 +0500

    add some limits on get methods

--- server.js
+++ server.js
@@ -11,13 +11,28 @@ const posts=[
     {
         id:2,
         title:'Some thing at second...'
-    }
+    },
+    {
+        id:3,
+        title:'Some thing at second...'
+    },
+    {
+        id:4,
+        title:'Some thing at second...'
+    },
+
 ]
 // setup static folder
 app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/api/posts",(req,res)=>{
-    res.send(posts);
+app.get('/api/posts',(req,res)=>{
+    const limit=parseInt(req.query.limit);
+    if(!isNaN(limit)&&limit>0){
+        const requiredResult=posts.slice(0,limit);
+        res.send(requiredResult);
+    }else{
+        res.send(posts);
+    }
 })
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"public","index.html"))
@@ -27,4 +42,10 @@ app.get("/about",(req,res)=>{
 })
 app.listen(port,()=>{
     console.log("Server is listening on port 5000");
+})
+app.get("/api/posts/:id",(req,res)=>{
+    const {id}=req.params;
+    const query=req.query;
+    const post=posts.find((item)=>item.id===parseInt(id));
+    res.send(post);
 })
