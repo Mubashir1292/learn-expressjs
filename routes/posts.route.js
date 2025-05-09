dc028a6082386299e1c1f6966420c7d8df993431
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-09 16:30:55 +0500

    add initial routes

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,1 +1,41 @@
 // ! posts routes...
+const express =require("express");
+const router=express.Router();
+let posts = [
+    {
+        id: 1,
+        title: 'Some thing at first'
+    },
+    {
+        id: 2,
+        title: 'Some thing at second...'
+    },
+    {
+        id: 3,
+        title: 'Some thing at second...'
+    },
+    {
+        id: 4,
+        title: 'Some thing at second...'
+    },
+
+]
+router.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit);
+    if (!isNaN(limit) && limit > 0) {
+        const requiredResult = posts.slice(0, limit);
+        return res.status(200).json(requiredResult);
+    }
+    res.status(200).json(posts);
+})
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const post = posts.find((item) => item.id === parseInt(id));
+    if (post) {
+      return res.status(200).json(post);
+    }
+    res.status(404).json({ message: `post not founded on id:${id}` });
+
+})
+
+module.exports =router;
--- server.js
+++ server.js
@@ -1,53 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express=require("express");
 const app = express();
 const port = process.env.port || 5001;
+const posts = require("./routes/posts.route.js");
 app.use(express.json());
-const posts = [
-    {
-        id: 1,
-        title: 'Some thing at first'
-    },
-    {
-        id: 2,
-        title: 'Some thing at second...'
-    },
-    {
-        id: 3,
-        title: 'Some thing at second...'
-    },
-    {
-        id: 4,
-        title: 'Some thing at second...'
-    },
 
-]
+app.use("/api/posts",posts)
 // setup static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        const requiredResult = posts.slice(0, limit);
-        return res.status(200).json(requiredResult);
-    }
-    res.status(200).json(posts);
-})
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-})
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "about.html"));
-})
+// app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/posts")
 app.listen(port, () => {
     console.log("Server is listening on port 5000");
-})
-app.get("/api/posts/:id", (req, res) => {
-    const { id } = req.params;
-    const post = posts.find((item) => item.id === parseInt(id));
-    if (post) {
-      return res.status(200).json(post);
-    }
-    res.status(404).json({ message: `post not founded on id:${id}` });
-
 })
